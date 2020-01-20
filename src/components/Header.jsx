d2824f1cf397d4c57c59fdcdc3fa7fcd15f52e2c
Danielle Rubin <dlhrubin@gmail.com>
2020-01-20 15:26:35 -0500

    Set up eslint, implement header

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="minor-nav">
+      <div>
+        <span style={{ fontWeight: 500, fontSize: '1.45em' }}>Google</span>
+        <span>Fonts</span>
+      </div>
+      <ul>
+        <li>
+          <button type="button" href="#">CATALOG</button>
+        </li>
+        <li>
+          <button type="button" href="#">FEATURED</button>
+        </li>
+        <li>
+          <button type="button" href="#">ARTICLES</button>
+        </li>
+        <li>
+          <button type="button" href="#">ABOUT</button>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
