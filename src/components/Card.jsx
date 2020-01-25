194a5ec18ae8fa7e379d1d903057ea9bc47b0f1a
Danielle Rubin <dlhrubin@gmail.com>
2020-01-25 18:59:41 -0500

    Fix text area sizing

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,8 +5,23 @@ class Card extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      displayed: 'Lorem ipsum dolor',
+      displayed: 'Then came the night of the first falling star.',
+      boxHeight: "",
+      lineHeight: "",
+      entered: false,
+      backspaced: false
     };
+    this.sizeSetter = React.createRef();
+    this.textArea = React.createRef();
+  }
+  
+  // Fetch height of shadow textarea element and set as textarea's height
+  componentDidMount() {
+    const sizeSetter = this.sizeSetter.current;
+    this.setState({
+      boxHeight: sizeSetter.clientHeight,
+      lineHeight: sizeSetter.clientHeight
+    });
   }
 
   // If user has typed in the "Type something box", display this text on the card
@@ -20,18 +35,43 @@ class Card extends Component {
     }
   }
 
-  // If user has typed into the card's text box, display this text on the card
+  // If user has typed into the card's text box, display this text and auto-resize text box
   handleChange = (e) => {
+    const sizeSetter = this.sizeSetter.current;
+    const textBox = this.textArea.current;
+    const {boxHeight, lineHeight, entered, backspaced} = this.state;
     this.setState({
       displayed: e.target.value,
+      boxHeight: !e.target.value ? lineHeight :
+                 entered ? boxHeight + lineHeight : 
+                 backspaced ? Math.min(sizeSetter.clientHeight, textBox.scrollHeight) :
+                 Math.max(sizeSetter.clientHeight, textBox.scrollHeight),
+      entered: false,
+      backspaced: false
     });
+  }
+
+  // Add new line to text box on enter and delete line(s) on backspace
+  handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      this.setState({
+        entered: true
+      });
+    } else if (e.key === "Backspace") {
+      this.setState({
+        backspaced: true
+      })
+    }
   }
 
   render() {
     const {
       name, styles,
     } = this.props;
-    const { displayed } = this.state;
+    const { displayed, boxHeight, lineHeight} = this.state;
+    const textStyle = {
+      height: "" ? 'auto' : boxHeight + "px"
+    }
     return (
       <div className="card">
         <div className="card-header">
@@ -47,7 +87,10 @@ class Card extends Component {
             <span>+</span>
           </button>
         </div>
-        <textarea value={displayed} onChange={this.handleChange} />
+        <div className="text-container">
+          <textarea value={displayed} style={textStyle} ref={this.textArea} onChange={this.handleChange} onKeyDown={this.handleKeyDown}/>
+          <span className="size-setter" style={{minHeight: lineHeight + "px"}} ref={this.sizeSetter}>{displayed}</span>
+        </div>
       </div>
     );
   }
